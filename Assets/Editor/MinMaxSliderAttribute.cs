fe3f23267aa3c4c8aa570d71748592f3ce25f16e
sikny <pamartqu@gmail.com>
2021-06-11 11:05:54 +0200

    first commit

---
+++ Assets/Editor/MinMaxSliderAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace UnityExtendedEditor.Editor {
+    public class MinMaxSliderAttribute : PropertyAttribute
+    {
+    
+    }
+}
