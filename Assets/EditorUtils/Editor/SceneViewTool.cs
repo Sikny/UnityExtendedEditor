0a60111c746a61c9895a3be99210885443cf936d
Quentin-AA <quentin@app-advisory.com>
2022-08-03 12:50:45 +0200

    sceneviewtool ordered axis + axis hidden when direction close to scene forward

--- Assets/EditorUtils/Editor/SceneViewTool.cs
+++ Assets/EditorUtils/Editor/SceneViewTool.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEditor.SceneManagement;
@@ -7,6 +9,11 @@ using UnityEngine;
 namespace UnityExtendedEditor.EditorUtils.Editor {
     [EditorTool("Scene View Tool")]
     public class SceneViewTool : EditorTool {
+        private struct ColoredAxis {
+            public Vector3 direction;
+            public Color color;
+        }
+
         public override void OnToolGUI(EditorWindow window) {
             var view = window as SceneView;
 
@@ -28,13 +35,23 @@ namespace UnityExtendedEditor.EditorUtils.Editor {
             var transform = ((GameObject)target).transform;
             var pos = transform.position;
             var size = HandleUtility.GetHandleSize(pos) / 2f;
-            
-            DrawAxisButton(pos, transform.up, size, Handles.yAxisColor);
-            DrawAxisButton(pos, transform.forward, size, Handles.zAxisColor);
-            DrawAxisButton(pos, transform.right, size, Handles.xAxisColor);
+
+            var axis = new[] {
+                new ColoredAxis { direction = transform.up, color = Handles.yAxisColor },
+                new ColoredAxis { direction = transform.forward, color = Handles.zAxisColor },
+                new ColoredAxis { direction = transform.right, color = Handles.xAxisColor },
+            };
+            axis = OrderDirectionByAngleWithSceneView(axis).ToArray();
+            foreach (var v in axis) {
+                DrawAxisButton(pos, v.direction, size, v.color);
+            }
         }
 
         private void DrawAxisButton(Vector3 pos, Vector3 dir, float size, Color col) {
+            var sceneCamForward = SceneView.currentDrawingSceneView.camera.transform.forward;
+            var angle = Vector3.Angle(sceneCamForward, -dir);
+            if (angle < 15) return;
+            
             Handles.color = col;
             if (Handles.Button(pos + dir * size, Quaternion.LookRotation(-dir), size, size, Handles.ConeHandleCap)) {
                 SetSceneViewLookAt(-dir);
@@ -44,6 +61,17 @@ namespace UnityExtendedEditor.EditorUtils.Editor {
         private void SetSceneViewLookAt(Vector3 direction) {
             var sceneView = SceneView.currentDrawingSceneView;
             sceneView.rotation = Quaternion.LookRotation(direction);
+        }
+
+        private static IEnumerable<ColoredAxis> OrderDirectionByAngleWithSceneView(IEnumerable<ColoredAxis> directions) {
+            var sceneDir = SceneView.currentDrawingSceneView.camera.transform.forward;
+            var result = new List<ColoredAxis>(directions);
+            result.Sort((d1, d2) => {
+                var angle1 = Vector3.Angle(sceneDir, d1.direction);
+                var angle2 = Vector3.Angle(sceneDir, d2.direction);
+                return angle1.CompareTo(angle2);
+            });
+            return result;
         }
     }
 }
